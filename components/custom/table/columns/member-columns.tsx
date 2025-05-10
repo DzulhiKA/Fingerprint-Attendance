19935d8504f20da2daf8c3d0a1e4e522b95e8eee
AndreFirmansyah06 <indomieseleramu1@gmail.com>
2025-05-10 10:44:16 +0700

    feat(api):penambahan validasi upload member

--- components/custom/table/columns/member-columns.tsx
+++ components/custom/table/columns/member-columns.tsx
@@ -227,6 +227,7 @@ export const memberColumn: ColumnDef<TMember>[] = [
                                 pwd,
                                 rfid,
                                 priv,
+                                expiredAt,
                                 tmp,
                               }),
                             }).then(async (res) => {
